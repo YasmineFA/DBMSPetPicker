3ac68ed0618afbf3c15e730810b5fbc0dcd162b6
Yasmine Aoua <yfaoua@wpi.edu>
2022-11-29 12:00:58 -0500

    updates to flask structure and untested functionality

--- src/App.py
+++ src/App.py
@@ -1,39 +1,122 @@
-from website import create_app
-from flask import Flask, render_template, redirect, url_for, request, flash
-app = create_app()
+# from website import create_app
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+from flask_mysqldb import MySQL
 
-# @app.route('/')
-# def index():
-#    return render_template('index.html')
+app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'CS542Team1'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = #'pfadmin@petfinderdb'
+# app.config['MYSQL_PASSWORD'] = 'pfapi2022!'
+# app.config['MYSQL_DB'] = 'guest'
 
-# @app.route('/login',methods = ['POST', 'GET'])
-# def login():
-#    if request.method == 'POST':
-#       user = request.form['fname']
-#       pwd = request.form['pwd']
-#       # check username and pwd are correct
-#       return redirect(url_for('account',name = user))
-#    else:
-#       return redirect(url_for('index'))
+mysql = MySQL(app)
 
-# @app.route('/signup',methods = ['POST', 'GET'])
-# def signup():
-#    if request.method == 'POST':
-#       user = request.form['fname']
-#       pwd = request.form['pwd']
-#       # check username isn't taken
-#       if(pwd.length > 8):
-#          flash("Account created", category='success')
-#          return redirect(url_for('account',name = user))
-#       else:
-#          flash("Password is too short, must be at least 8 characters", category='error')
-#          return redirect(url_for('index'))
-#    else:
-#       return redirect(url_for('index'))
+@app.route('/')
+def index():
+   return render_template('index.html')
 
-# @app.route("/account/<user>")
-# def account(user):
-#    pass
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['fname']
+      pwd = request.form['pwd']
+      if request.form['authorize'] == "Login":
+         # print(user)
+         # print(pwd)
+         # check username and pwd are correct
+         if(auth_login(user, pwd)):
+            return redirect(url_for('account',user = user))
+         else:
+            flash("Incorrect login, please try again", category='error')
+            return redirect(url_for('index'))
+
+      elif request.form['authorize'] == "Register":
+         # check username isn't taken
+         if(sign_up(user, pwd)):
+            flash("Account created", category='success')
+            return redirect(url_for('account', user = user))
+         else:
+            return redirect(url_for('index'))
+      else:
+         return redirect(url_for('index'))
+   else:
+      return redirect(url_for('index'))
+
+@app.route("/account/<user>")
+def account(user):
+   queryList = getSavedSearches(user)
+   return render_template('account.html', username=user, searches=queryList)
+
+@app.route("/results/<query>", methods=['POST'])
+def results(query):
+   return render_template('results.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+def connectDB():
+   conn = mysql.connector.connect(
+         host="localhost",
+         user="root",
+         password="root",
+         database="petfinderDB"
+         )
+   return conn
+
+def auth_login(user, pwd):
+   conn = connectDB()
+   cur = conn.cursor()
+   cur.execute("SELECT * FROM users WHERE username = % s AND password = % s", (user, pwd))
+   account = cur.fetchone()
+   if account:
+      session['loggedin'] = True
+      session['id'] = account['id']
+      session['username'] = account['username']
+      cur.close()
+      conn.close()
+      return True
+   else:
+      cur.close()
+      conn.close()
+      return False
+
+def sign_up(user, pwd):
+   # check password meets requirements
+   if(len(pwd) < 8):
+      flash("Password is too short", category='error')
+      return False
+   # check username isn't taken
+   conn = connectDB()
+   cur = conn.cursor()
+   cur.execute("SELECT * FROM user WHERE username = % s AND password = % s", (user, pwd))
+   accExists = cur.fetchone()
+   if(accExists==None):
+      # add account info into users table
+      cur.execute("INSERT INTO user WHERE username = % s AND password = % s", (user, pwd))
+      conn.commit()
+      session['loggedin'] = True
+      session['id'] = account['id']
+      session['username'] = account['username']
+      cur.close()
+      conn.close()
+      return True
+   else:
+      flash("Username already taken", category='error')
+      cur.close()
+      conn.close()
+      return False
+
+def getSavedSearches(user):
+   conn = connectDB()
+   cur = conn.cursor()
+   cur.execute("SELECT searchQuery FROM user WHERE username= % s", (user))
+   searches = cur.fetchone()
+   queryList = searches.split(', ')
+   return queryList 
 
 if __name__ == '__main__':
    app.run(debug=True)
--- src/website/__init__.py
+++ src/website/__init__.py
@@ -1,37 +1,37 @@
-from flask import Flask
-import mysql.connector
-from mysql.connector import errorcode
+# from flask import Flask
+# # from flask_mysqldb import MySQL
+# import mysql.connector
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'CS542Team1'
+# config = {
+#     # 'host':'petfinderdb.mysql.database.azure.com',
+#     'host' : 'localhost',
+#     'user':'pfadmin@petfinderdb',
+#     'password':'pfapi2022!',
+#     'database':'guest',
+#     'client_flags': [mysql.connector.ClientFlag.SSL],
+#     'ssl_ca': '/var/www/html/BaltimoreCyberTrustRoot.crt.pem'
+# }
+# db = mysql.connector.connect(**config)
 
-    config = {
-    'host':'petfinderdb.mysql.database.azure.com',
-    'user':'pfadmin>@petfinderdb',
-    'password':'pfapi2022!',
-    'database':'guest',
-    'client_flags': [mysql.connector.ClientFlag.SSL],
-    'ssl_ca': '/var/www/html/BaltimoreCyberTrustRoot.crt.pem'
-    }
-
-    try:
-        conn = mysql.connector.connect(**config)
-        print("Connection established")
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with the user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
-    else:
-        cursor = conn.cursor()
+# # mysql = MySQL(app)
 
 
-    from .views import views
-    from .auth import auth
 
-    app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
-    return app
+# # try:
+# #     conn = mysql.connector.connect(config)
+# #     print("Connection established")
+# # except mysql.connector.Error as err:
+# #     print(err)
+# # else:
+# #     cursor = conn.cursor()
+
+# def create_app():
+#     app = Flask(__name__)
+#     app.config['SECRET_KEY'] = 'CS542Team1'
+
+#     from .views import views
+#     from .auth import auth
+
+#     app.register_blueprint(views, url_prefix="/")
+#     app.register_blueprint(auth, url_prefix="/")
+#     return app
